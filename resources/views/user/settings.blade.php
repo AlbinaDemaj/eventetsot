68eb81700ae0b73b9c3efdf2c29c206a43448c46
Oneeb Arif <oneeb.arif55@gmail.com>
2025-06-12 02:43:55 +0500

    ui issue

--- resources/views/user/settings.blade.php
+++ resources/views/user/settings.blade.php
@@ -141,130 +141,140 @@
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-              <div class="welcome-tabbing">
-                <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                      <button class="nav-link active" id="pills-appearance-tab" data-bs-toggle="pill" data-bs-target="#pills-appearance" type="button" role="tab" aria-controls="pills-appearance" aria-selected="true">Appearance</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                      <button class="nav-link" id="pills-guest-tab" data-bs-toggle="pill" data-bs-target="#pills-guest" type="button" role="tab" aria-controls="pills-guest" aria-selected="false">Guest Form</button>
-                    </li>
-                    
-                </ul>
-                <div class="tab-content" id="pills-tabContent" style="display: block">
-                    <div class="tab-pane fade show active" id="pills-appearance" role="tabpanel" aria-labelledby="pills-appearance-tab">
-                        <div class="row">   
-                            <div class="col-md-8">
-                                <div class="welcome-screen-appearnce">
-                                    <div class="row">
-                                        <div class="col-md-8">
-                                            <div class="form-group">
-                                                <label for="event_date">Title</label>
-                                                <p>Enter the main title for your welcome screen.</p>
-                                                <div class="form-area">
-                                                    <div class="form-group">
-                                                        <div class="input-wrapper">
-                                                            <input type="text" value="Oneeb Wed" required>
+                <div class="welcome-tabbing">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                                <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="pills-appearance-tab" data-bs-toggle="pill" data-bs-target="#pills-appearance" type="button" role="tab" aria-controls="pills-appearance" aria-selected="true">Appearance</button>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                <button class="nav-link" id="pills-guest-tab" data-bs-toggle="pill" data-bs-target="#pills-guest" type="button" role="tab" aria-controls="pills-guest" aria-selected="false">Guest Form</button>
+                                </li>
+                            </ul>
+                            <div class="tab-content" id="pills-tabContent" style="display: block">
+                                <div class="tab-pane fade show active" id="pills-appearance" role="tabpanel" aria-labelledby="pills-appearance-tab">
+                                    <div class="welcome-screen-appearnce">
+                                        <div class="row">
+                                            <div class="col-md-8">
+                                                <div class="form-group">
+                                                    <label for="event_date">Title</label>
+                                                    <p>Enter the main title for your welcome screen.</p>
+                                                    <div class="form-area">
+                                                        <div class="form-group">
+                                                            <div class="input-wrapper">
+                                                                <input type="text" value="Oneeb Wed" required>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-4">
-                                            <div class="form-group">
-                                                <label for="event_date"></label>
-                                                <p class="pt-1">Choose font:</p>
-                                                <div class="form-area">
-                                                    <div class="form-group">
-                                                        <div class="input-wrapper">
-                                                            <input type="text" value="Font Name" required>
+                                            <div class="col-md-4">
+                                                <div class="form-group">
+                                                    <label for="event_date"></label>
+                                                    <p class="pt-1">Choose font:</p>
+                                                    <div class="form-area">
+                                                        <div class="form-group">
+                                                            <div class="input-wrapper">
+                                                                <input type="text" value="Font Name" required>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="form-group">
-                                                <label for="event_date">Description</label>
-                                                <p>Add a introduction text below the main title.</p>
-                                                <div class="form-area">
-                                                    <div class="form-group">
-                                                        <div class="input-wrapper">
-                                                            <input type="text" value="Share your photos & videos with us!" required>
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label for="event_date">Description</label>
+                                                    <p>Add a introduction text below the main title.</p>
+                                                    <div class="form-area">
+                                                        <div class="form-group">
+                                                            <div class="input-wrapper">
+                                                                <input type="text" value="Share your photos & videos with us!" required>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-5">
-                                            <div class="form-group">
-                                                <label for="event_date">Button</label>
-                                                <p>Set the text of the submission button.</p>
-                                                <div class="form-area">
-                                                    <div class="form-group">
-                                                        <div class="input-wrapper">
-                                                            <input type="text" value="Continue" required>
+                                            <div class="col-md-5">
+                                                <div class="form-group">
+                                                    <label for="event_date">Button</label>
+                                                    <p>Set the text of the submission button.</p>
+                                                    <div class="form-area">
+                                                        <div class="form-group">
+                                                            <div class="input-wrapper">
+                                                                <input type="text" value="Continue" required>
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div class="col-md-12">
-                                            <div class="row">
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="event_date">Background</label>
-                                                        <p>Set a background for the welcome screen.</p>
-                                                        <li class="animation-checkbox">
-                                                            <input class="styled-checkbox" id="styled-checkbox-1" type="checkbox" value="value1">
-                                                            <label for="styled-checkbox-1">Animated</label>
-                                                        </li>
+                                            <div class="col-md-12">
+                                                <div class="row">
+                                                    <div class="col-md-6">
+                                                        <div class="form-group">
+                                                            <label for="event_date">Background</label>
+                                                            <p>Set a background for the welcome screen.</p>
+                                                            <li class="animation-checkbox">
+                                                                <input class="styled-checkbox" id="styled-checkbox-1" type="checkbox" value="value1">
+                                                                <label for="styled-checkbox-1">Animated</label>
+                                                            </li>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="change-bg">
-                                                        <img src="../user/assets/img/colored-paper.jpg" />
-                                                        <button class="btn change-bg-btn">Change</button>
+                                                    <div class="col-md-6">
+                                                        <div class="change-bg">
+                                                            <img src="../user/assets/img/colored-paper.jpg" />
+                                                            <button class="btn change-bg-btn">Change</button>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4">
-                                <div class="welcomescreen-mbl-view">
-                                    <div class="center-card">
-                                        <div class="card-caption">
-                                            <div class="img-wrap">
-                                                <img src="{{ asset('website/img/img-round.jpg') }}">
-                                            </div>
-                                            <h4 class="m-0">Sam Dasma</h4>
-                                            <p>Ju lutemi ndani fotot dhe videot tuaja me ne për këtë ditë të veçantë!</p>
-                                            <form>
-                                                <div class="input-wrap">
-                                                    <input type="text" class="form-control" name="name" placeholder="Shkruani Emrin Këtu">
-                                                    <div class="submit-wrap">
-                                                        <input type="button" name="continue" >
-                                                    </div>
+                                
+                                <div class="tab-pane fade" id="pills-guest" role="tabpanel" aria-labelledby="pills-guest-tab">
+                                  <div class="guest-tab-detail">
+                                    <p>Create a form to collect guest information before they proceed to your album. Add new fields as needed, or rearrange existing ones by dragging.</p>
+                                    <div class="form-group">
+                                        <div class="form-area">
+                                            <div class="form-group">
+                                                <div class="input-wrapper">
+                                                    <input type="text" value="" placeholder="Text" required>
                                                 </div>
-                                            </form>
+                                            </div>
                                         </div>
+                                    </div>
+                                  </div>
+                                </div>
+                            </div>
+                        </div>    
+                        <div class="col-md-4">
+                            <div class="welcomescreen-mbl-view">
+                                <div class="center-card">
+                                    <div class="card-caption">
+                                        <div class="img-wrap">
+                                            <img src="{{ asset('website/img/img-round.jpg') }}">
+                                        </div>
+                                        <h4 class="m-0">Sam Dasma</h4>
+                                        <p>Share your photo & video with us</p>
+                                        <form>
+                                            <div class="input-wrap">
+                                                <input type="text" class="form-control" name="name" placeholder="Shkruani Emrin Këtu">
+                                                <div class="submit-wrap">
+                                                    <button type="button" class="login-btn">Continue </button>
+                                                </div>
+                                            </div>
+                                        </form>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane fade" id="pills-guest" role="tabpanel" aria-labelledby="pills-guest-tab">
-                        sdfsfdgdfgsf
-                    </div>
-                
                 </div>
-              </div>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Send message</button>
+            <div class="modal-footer welcome-modal-ft">
+              
+              <button type="button" class="btn login-btn">Save</button>
             </div>
           </div>
         </div>
