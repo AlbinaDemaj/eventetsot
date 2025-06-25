c37894a99ff45acb2fdba13a7b81543dfc1aa49e
Oneeb Arif <oneeb.arif55@gmail.com>
2025-06-25 18:36:21 +0500

    video changes

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -100,7 +100,7 @@
             </div>
             <div class="row how-works-box align-items-center">
                 <div class="col-md-6 d-md-block d-none">
-                    <video class="appDemo mw-100" autoplay="" loop="" muted="" playsinline="" poster="{{ asset('website/img/sidevidimg-1.png') }}" src="{{ asset('website/img/sidevid-2.webm') }}"></video>
+                    <video class="appDemo mw-100" autoplay="" loop="" muted="" playsinline="" poster="{{ asset('website/img/sidevidimg-1.png') }}" src="{{ asset('website/img/sidevid-2.mp4') }}"></video>
                 </div>
                 <div class="tbox col-md-6">
                     <span class="ecount">2</span>
